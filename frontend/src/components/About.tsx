3df82277d637fcebef6eb0e459b7f3aee64a14c2
manas <manasnanivadekar@gmail.com>
2024-09-02 22:59:14 +0530

    minor bug fixes

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -4,10 +4,7 @@ import image from "/image1.jpg";
 
 export const About = () => {
   return (
-    <section
-      id="about"
-      className="container py-24 sm:py-32"
-    >
+    <section id="about" className="container py-24 sm:py-32">
       <div className="bg-background border rounded-lg py-12">
         <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
           <img
@@ -24,7 +21,13 @@ export const About = () => {
                 Majs
               </h2>
               <p className="text-xl text-muted-foreground mt-4">
-                Majs is a platform that simplifies application deployment across multiple cloud providers. It allows you to deploy your applications with a single click, without having to worry about the underlying infrastructure. With features like automated dependency management and customizable deployment flows, Majs streamlines the entire process, catering to both beginners and experienced developers.
+                Majs is a platform that simplifies application deployment across
+                multiple cloud providers. It allows you to deploy your
+                applications with a single click, without having to worry about
+                the underlying infrastructure. With features like automated
+                dependency management and customizable deployment flows, Majs
+                streamlines the entire process, catering to both beginners and
+                experienced developers!
               </p>
             </div>
 
